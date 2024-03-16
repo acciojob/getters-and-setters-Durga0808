81435e1a7570d31157c299023d78f8311ab075c9
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-03-16 20:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-//complete this code
 class Person {
-	constructor(name, age){
-		this.name=name;
-		this._age = age;
-	}
-	getname(){
-		return this.name;
-	}
-
-	setage(newAge){
-		this._age = newAge;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name(){
+    return this._name;
+  }
+  set age(newAge){
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-	constructor(name, age){
-		super(name,age);
-	}
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+  constructor(name, age){
+    super(name,age);
+  }
+  study() {
+     console.log(this.name + " is studying");
+  }
 }
 
 class Teacher extends Person {
-	constructor(name, age){
-		super(name,age);
-	}
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  constructor(name, age){
+    super(name,age);
+  }
+  teach() {
+     console.log(this.name + " is teaching");
+  }
 }
+
+const person = new Person("John", 25);
+console.log(person.name); 
+
+person.age = 30;
+console.log(person._age);
+
+const student = new Student("John", 22);
+const teacher = new Teacher("Alice", 40);
+student.study();
+teacher.teach();
+
+const student1 = new Student("Alice", 22);
+const teacher1 = new Teacher("John", 40);
+student1.study();
+teacher1.teach();
 
 // Do not change the code below this line
 window.Person = Person;
